ece1f1704bcfe3820d2aadb9e8afc8c2f820a1f4
kkkimgyuri <kimgyuri@gimgyuliui-MacBookPro.local>
2021-09-15 15:26:32 +0900

    Updates

---
+++ precache-manifest.50dcec75a55e4f28ca3bf35ab3c71252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c70d643a0ca3076880a58402a13905d",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "c1e6f1a63ae1b508f987",
+    "url": "/nwitter/static/css/main.b269568b.chunk.css"
+  },
+  {
+    "revision": "46f547b950ed039e4a5b",
+    "url": "/nwitter/static/js/2.e02bddeb.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/nwitter/static/js/2.e02bddeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e6f1a63ae1b508f987",
+    "url": "/nwitter/static/js/main.c86e8f30.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
