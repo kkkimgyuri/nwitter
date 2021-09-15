24d3227eaf343c0c770a62e7a3415c189acbb480
kkkimgyuri <kimgyuri@gimgyuliui-MacBookPro.local>
2021-09-15 15:50:51 +0900

    Updates

--- precache-manifest.c2ccf42601ae63bcaa2fa2d3ec491018.js
+++ precache-manifest.c2ccf42601ae63bcaa2fa2d3ec491018.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ed9065031636818296f07fed269d421",
+    "revision": "1be5f85c020a6fb9707c3b24b1bf0d95",
     "url": "/nwitter/index.html"
   },
   {
-    "revision": "644b0a7aa79584571ba0",
-    "url": "/nwitter/static/css/main.1ad4efb2.chunk.css"
+    "revision": "5056a0cfd25d88347fe0",
+    "url": "/nwitter/static/css/main.8e5c9bcd.chunk.css"
   },
   {
     "revision": "443afafa130a52a9f63d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nwitter/static/js/2.30fcab9e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "644b0a7aa79584571ba0",
-    "url": "/nwitter/static/js/main.0cc0d49d.chunk.js"
+    "revision": "5056a0cfd25d88347fe0",
+    "url": "/nwitter/static/js/main.fd8f4821.chunk.js"
   },
   {
     "revision": "97fb235846154051176c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nwitter/precache-manifest.9776b74c2b0e331e0d37ffc087d44eb5.js"
+  "/nwitter/precache-manifest.c2ccf42601ae63bcaa2fa2d3ec491018.js"
 );
 
 self.addEventListener('message', (event) => {
